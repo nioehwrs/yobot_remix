7ee9fc6237e9532ed6477307381e2d362e9eaeee
nioehwrs <nioehwrs@kotori.moe>
2026-01-04 18:11:25 +0800

    refactor: 优化排刀页面代码结构
    - 添加CONFIG配置常量集中管理
    - 重构syncFromPreviousDay减少重复代码
    - 添加版本号自动生成函数 getVersion()
    - 使用Vue.

--- src/client/public/static/clan/arrange.js
+++ src/client/public/static/clan/arrange.js
@@ -1,14 +1,64 @@
+var CONFIG = {
+    X: { min: 5, max: 40, defaultDay1: 14, defaultDay2to5: 18 },
+    A: { options: [1, 2, 3, 4, 5, 6] },
+    B: { options: [0, 1, 2, 3] },
+    C: { options: [2, 3, 4] },
+    E: { day1: 70, day2to5: 90 },
+    DAYS: [1, 2, 3, 4, 5],
+    BOSSES: [1, 2, 3, 4, 5]
+};
+
+var VERSION = '2.1.0';
+
+function getVersion() {
+    return VERSION;
+}
+
+function generateVersion() {
+    var now = new Date();
+    var major = 2;
+    var minor = now.getFullYear() - 2024;
+    var patch = now.getMonth() + 1;
+    return major + '.' + minor + '.' + patch;
+}
+
 var vm = new Vue({
     el: '#app',
     data: {
         knifeList: { 2: [], 3: [], 4: [] },
         activeIndex: '2',
+        bossData: { 1:{icon_id:0}, 2:{icon_id:0}, 3:{icon_id:0}, 4:{icon_id:0}, 5:{icon_id:0} },
+        base_cycle: 1,
+        days: CONFIG.DAYS,
+        bosses: CONFIG.BOSSES,
+        statsConfig: {},
+        dayStats: {}
     },
     mounted() {
         document.title = '排刀列表 - 公会战';
         this.loadKnifeData();
+        this.loadBossData();
+        this.initStatsConfig();
+        this.loadStatsData();
+        this.loadDayStatsData();
     },
     methods: {
+        loadBossData() {
+            var thisvue = this;
+            axios.post("../api/", {
+                action: 'get_data',
+                csrf_token: csrf_token,
+            }).then(function (res) {
+                if (res.data.code == 0) {
+                    thisvue.bossData = res.data.bossData;
+                    thisvue.base_cycle = res.data.groupData.cycle;
+                } else {
+                    console.error('加载boss数据失败:', res.data.message);
+                }
+            }).catch(function (error) {
+                console.error('加载boss数据错误:', error);
+            });
+        },
         loadKnifeData() {
             var thisvue = this;
             console.log('开始加载刀型数据...');
@@ -66,6 +116,10 @@ var vm = new Vue({
         renderKnifeData() {
             var emptyDiv = document.getElementById('knife-empty');
             var container = document.getElementById('knife-container');
+            
+            if (!emptyDiv || !container) {
+                return;
+            }
             
             var totalCount = (this.knifeList[2] || []).length + (this.knifeList[3] || []).length + (this.knifeList[4] || []).length;
             
@@ -207,24 +261,6 @@ var vm = new Vue({
                 this.renderPhaseKnives(phase);
             }
         },
-        renderPhaseKnives(phase) {
-            var phaseContainer = document.getElementById('knife-phase-' + phase);
-            if (!phaseContainer) {
-                return;
-            }
-            var phaseList = this.knifeList[phase] || [];
-            var emptyDiv = document.getElementById('knife-phase-' + phase + '-empty');
-            
-            phaseContainer.innerHTML = '';
-            
-            if (emptyDiv) {
-                emptyDiv.style.display = phaseList.length === 0 ? 'block' : 'none';
-            }
-            
-            for (var i = 0; i < phaseList.length; i++) {
-                phaseContainer.insertAdjacentHTML('beforeend', this.generatePhaseKnifeHtml(phase, i, phaseList[i]));
-            }
-        },
         updateKnife(phase, index, field, value) {
             if (this.knifeList[phase] && this.knifeList[phase][index]) {
                 if (field === 'boss_num' || field === 'value') {
@@ -264,10 +300,237 @@ var vm = new Vue({
                     break;
             }
         },
+        initStatsConfig() {
+            for (var day = 1; day <= 5; day++) {
+                this.statsConfig[day] = {};
+                for (var boss = 1; boss <= 5; boss++) {
+                    var defaultX = day === 1 ? CONFIG.X.defaultDay1 : CONFIG.X.defaultDay2to5;
+                    this.statsConfig[day][boss] = {
+                        a: 1,
+                        b: 0,
+                        c: 2,
+                        x: defaultX,
+                        tail: 'full'
+                    };
+                }
+            }
+            this.initDayStats();
+        },
+        initDayStats() {
+            for (var day = 1; day <= 5; day++) {
+                this.dayStats[day] = { locked: false, usePrevious: false };
+            }
+        },
+        updateStats(day, boss, field, value) {
+            if (!this.statsConfig[day]) {
+                this.$set(this.statsConfig, day, {});
+            }
+            if (!this.statsConfig[day][boss]) {
+                this.$set(this.statsConfig[day], boss, { a: 1, b: 0, c: 2, x: 20, tail: 'full' });
+            }
+            
+            var currentConfig = this.statsConfig[day][boss];
+            
+            if (field === 'b') {
+                var newB = parseInt(value) || 0;
+                this.$set(currentConfig, 'b', newB);
+                if (newB >= currentConfig.c) {
+                    this.$set(currentConfig, 'c', newB + 1);
+                }
+                document.getElementById('c-select-' + day + '-' + boss).value = currentConfig.c;
+            } else if (field === 'c') {
+                var newC = parseInt(value) || 2;
+                this.$set(currentConfig, 'c', newC);
+                if (currentConfig.b >= newC) {
+                    this.$set(currentConfig, 'b', newC - 1);
+                }
+                document.getElementById('b-select-' + day + '-' + boss).value = currentConfig.b;
+            } else if (field === 'a') {
+                this.$set(currentConfig, 'a', parseInt(value) || 1);
+            } else if (field === 'x') {
+                var newX = parseInt(value) || 20;
+                if (newX < 5) newX = 5;
+                if (newX > 40) newX = 40;
+                this.$set(currentConfig, 'x', newX);
+                document.getElementById('x-input-' + day + '-' + boss).value = newX;
+            } else if (field === 'tail') {
+                this.$set(currentConfig, 'tail', value);
+            }
+            this.saveStatsData();
+        },
+        saveStatsData() {
+            var thisvue = this;
+            axios.post("../api/", {
+                action: 'save_stats_config',
+                statsConfig: thisvue.statsConfig,
+                csrf_token: csrf_token,
+            }).then(function (res) {
+                if (res.data.code == 0) {
+                    console.log('统计配置保存成功');
+                } else {
+                    console.error('保存统计配置失败:', res.data.message);
+                }
+            }).catch(function (error) {
+                console.error('保存统计配置错误:', error);
+            });
+        },
+        loadStatsData() {
+            var thisvue = this;
+            axios.post("../api/", {
+                action: 'get_stats_config',
+                csrf_token: csrf_token,
+            }).then(function (res) {
+                if (res.data.code == 0 && res.data.statsConfig) {
+                    thisvue.statsConfig = res.data.statsConfig;
+                } else {
+                    thisvue.initStatsConfig();
+                }
+            }).catch(function (error) {
+                console.error('加载统计配置错误:', error);
+                thisvue.initStatsConfig();
+            });
+        },
+        getDayFTotal(day) {
+            if (!this.statsConfig[day]) {
+                return 0;
+            }
+            var total = 0;
+            for (var boss = 1; boss <= 5; boss++) {
+                if (this.statsConfig[day][boss] && this.statsConfig[day][boss].x) {
+                    total += this.statsConfig[day][boss].x;
+                }
+            }
+            return total;
+        },
+        getDayFClass(day) {
+            var expectedE = day === 1 ? 70 : 90;
+            var totalF = this.getDayFTotal(day);
+            if (totalF < expectedE) {
+                return 'less';
+            } else if (totalF > expectedE) {
+                return 'more';
+            }
+            return 'normal';
+        },
+        getExpectedE(day) {
+            return day === 1 ? 70 : 90;
+        },
+        toggleDayLock(day) {
+            if (!this.dayStats[day]) {
+                this.$set(this.dayStats, day, { locked: false, usePrevious: false });
+            }
+            var newLocked = !this.dayStats[day].locked;
+            this.$set(this.dayStats[day], 'locked', newLocked);
+            if (newLocked) {
+                this.$set(this.dayStats[day], 'usePrevious', false);
+            }
+            this.saveDayStatsData();
+        },
+        toggleUsePrevious(day) {
+            if (!this.dayStats[day]) {
+                this.$set(this.dayStats, day, { locked: false, usePrevious: false });
+            }
+            if (this.dayStats[day].locked) {
+                return;
+            }
+            var newUsePrevious = !this.dayStats[day].usePrevious;
+            this.$set(this.dayStats[day], 'usePrevious', newUsePrevious);
+            if (newUsePrevious && day > 1) {
+                this.copyFromPreviousDay(day);
+            }
+            this.saveDayStatsData();
+        },
+        copyFromPreviousDay(day) {
+            if (day <= 1) return;
+            var prevDay = day - 1;
+            if (!this.statsConfig[prevDay]) return;
+            if (!this.statsConfig[day]) {
+                this.$set(this.statsConfig, day, {});
+            }
+            for (var boss = 1; boss <= 5; boss++) {
+                if (this.statsConfig[prevDay][boss]) {
+                    var defaultX = day === 1 ? CONFIG.X.defaultDay1 : CONFIG.X.defaultDay2to5;
+                    this.$set(this.statsConfig[day], boss, {
+                        a: this.statsConfig[prevDay][boss].a || 1,
+                        b: this.statsConfig[prevDay][boss].b || 0,
+                        c: this.statsConfig[prevDay][boss].c || 2,
+                        x: this.statsConfig[prevDay][boss].x || defaultX,
+                        tail: this.statsConfig[prevDay][boss].tail || 'full'
+                    });
+                }
+            }
+            this.saveStatsData();
+        },
+        syncFromPreviousDay(day) {
+            if (day <= 1) return;
+            var prevDay = day - 1;
+            if (!this.statsConfig[prevDay]) return;
+            if (!this.statsConfig[day]) {
+                this.$set(this.statsConfig, day, {});
+            }
+            for (var boss = 1; boss <= 5; boss++) {
+                if (this.statsConfig[prevDay][boss]) {
+                    if (!this.statsConfig[day][boss]) {
+                        this.$set(this.statsConfig[day], boss, {});
+                    }
+                    var defaultX = day === 1 ? CONFIG.X.defaultDay1 : CONFIG.X.defaultDay2to5;
+                    var source = this.statsConfig[prevDay][boss];
+                    this.$set(this.statsConfig[day][boss], 'a', source.a || 1);
+                    this.$set(this.statsConfig[day][boss], 'b', source.b || 0);
+                    this.$set(this.statsConfig[day][boss], 'c', source.c || 2);
+                    this.$set(this.statsConfig[day][boss], 'x', source.x || defaultX);
+                    this.$set(this.statsConfig[day][boss], 'tail', source.tail || 'full');
+                }
+            }
+        },
+        saveDayStatsData() {
+            var thisvue = this;
+            axios.post("../api/", {
+                action: 'save_day_stats',
+                dayStats: thisvue.dayStats,
+                csrf_token: csrf_token,
+            }).then(function (res) {
+                if (res.data.code == 0) {
+                    console.log('日统计状态保存成功');
+                } else {
+                    console.error('保存日统计状态失败:', res.data.message);
+                }
+            }).catch(function (error) {
+                console.error('保存日统计状态错误:', error);
+            });
+        },
+        loadDayStatsData() {
+            var thisvue = this;
+            axios.post("../api/", {
+                action: 'get_day_stats',
+                csrf_token: csrf_token,
+            }).then(function (res) {
+                if (res.data.code == 0 && res.data.dayStats) {
+                    thisvue.dayStats = res.data.dayStats;
+                } else {
+                    thisvue.initDayStats();
+                }
+            }).catch(function (error) {
+                console.error('加载日统计状态错误:', error);
+                thisvue.initDayStats();
+            });
+        },
+        initDayStats() {
+            for (var day = 1; day <= 5; day++) {
+                if (!this.dayStats[day]) {
+                    this.$set(this.dayStats, day, { locked: false, usePrevious: false });
+                }
+            }
+        }
     },
     watch: {
-        knifeList: {
-            handler(newVal, oldVal) {
+        statsConfig: {
+            handler(newStatsConfig, oldStatsConfig) {
+                for (var day = 2; day <= 5; day++) {
+                    if (this.dayStats[day] && this.dayStats[day].usePrevious) {
+                        this.syncFromPreviousDay(day);
+                    }
+                }
             },
             deep: true
         }
